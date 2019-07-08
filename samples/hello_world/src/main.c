e600953fcf3a09b991fe56d23781c9f42a7ec5dc
amina <aminakacem.isitcom@gmail.com>
2019-07-08 08:51:40 +0100

    version1

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -6,11 +6,38 @@
 
 #include <zephyr.h>
 #include <sys/printk.h>
+//#include <stm32f769i_discovery.h>
+//#include <stm32f769i_discovery_lcd.h>
+//#include <stm32f769i_discovery_sdram.h>
 #include <display/stm32f7-otm8009a.h>
 
 void main(void)
 { 
-	printk("Hello World! %s\n", CONFIG_BOARD);
-	init_test();
 
+test_func();
+
+
+while(1);
+
+	//printk("Hello World! %s\n", CONFIG_BOARD);
+	//init_test();
+/*
+  BSP_LCD_Init();
+  BSP_LCD_LayerDefaultInit(1, LCD_FB_START_ADDRESS);
+  
+  
+  BSP_LCD_SelectLayer(1);
+
+  BSP_LCD_SetFont(&LCD_DEFAULT_FONT);
+  
+  
+  BSP_LCD_SetBackColor(LCD_COLOR_WHITE); 
+  BSP_LCD_Clear(LCD_COLOR_WHITE);
+
+
+  BSP_LCD_SetTextColor(LCD_COLOR_DARKBLUE);  
+
+
+  BSP_LCD_DisplayStringAt(0, 10, (uint8_t *)"STM32F769I HELLO lcd", CENTER_MODE);
+*/
 }
